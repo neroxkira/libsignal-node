69117d135b3c63e17cae87e29018e231d16d933b
neroxava <blsmk.230127045@budiluhur.sch.id>
2026-01-01 00:28:40 +0700

    update license

--- src/curve.js
+++ src/curve.js
@@ -1,9 +1,34 @@
+/**
+ * ╔═══════════════════════════════════════════════════════════╗
+ * ║              NEROX - WhatsApp API Library                 ║
+ * ║                    Premium Edition                        ║
+ * ╚═══════════════════════════════════════════════════════════╝
+ * 
+ * @package     nerox-bails
+ * @version     0.1.0
+ * @author      NEROX Development Team
+ * @copyright   Copyright (c) 2026 NEROX. All rights reserved.
+ * @license     MIT License
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ */
 
 'use strict';
 
 const curveJs = require('curve25519-js');
 const nodeCrypto = require('crypto');
-/* External source: https://github.com/digitalbazaar/x25519-key-agreement-key-2019/blob/master/lib/crypto.js */
 const PUBLIC_KEY_DER_PREFIX = Buffer.from([
     48, 42, 48, 5, 6, 3, 43, 101, 110, 3, 33, 0
 ]);
